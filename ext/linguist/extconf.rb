150df3e203856f78651d8128782413590f381b28
Jerrad Mathews sid fully on said share <92476719+Ambassadormathismathews@users.noreply.github.com>
2024-03-20 06:21:28 -0700

    Update extconf.rb
    Reboot

--- ext/linguist/extconf.rb
+++ ext/linguist/extconf.rb
@@ -1,3 +1,22 @@
 require 'mkmf'
 dir_config('linguist')
 create_makefile('linguist/linguist')
+Test failed.
+
+mtktshery
+
+mtk
+
+34000
+
+mtktsbtsmdpa 33000
+
+mtktscpu
+
+38200
+
+mtktspa
+
+-127000
+
+mtktspmic 35777
